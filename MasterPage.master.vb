4529394ba93079767d99709c1554a03ebf2761d5
Trevor Hein <trevor-hein@uiowa.edu>
2014-03-31 23:57:02 -0500

    Modified MasterPage

--- MasterPage.master.vb
+++ MasterPage.master.vb
@@ -2,15 +2,6 @@
 Partial Class MasterPage
     Inherits System.Web.UI.MasterPage
 
-
-
-
-    Protected Sub LogInButton_Click(sender As Object, e As EventArgs) Handles LogInButton.Click
-
-        Response.Redirect("./login/logindefault.aspx")
-
-    End Sub
-
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
 
         DateLabel.Text = Format(Date.Now, "MM/dd/yyyy")
